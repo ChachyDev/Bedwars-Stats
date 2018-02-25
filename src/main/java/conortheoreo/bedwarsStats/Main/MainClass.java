749e781d380e28aa8344085ba63616222da11699
Conor <31479541+ConorTheDev@users.noreply.github.com>
2018-02-25 21:18:18 +0000

    fixed the code for the MainClass

--- src/main/java/conortheoreo/bedwarsStats/Main/MainClass.java
+++ src/main/java/conortheoreo/bedwarsStats/Main/MainClass.java
@@ -1,14 +1,15 @@
-package conortheoreo.bedwarsStats.main;
+package conortheoreo.bedwarsStats.Main;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = Main.MODID, version = Main.VERSION)
-public class Main
+@Mod(modid = MainClass.MODID, version = MainClass.VERSION)
+public class MainClass
 {
-    public static final String MODID = "BedwarsStats";
+    public static final String MODID = "bedwars-stats";
     public static final String VERSION = "1";
 
+    //todo
 }
