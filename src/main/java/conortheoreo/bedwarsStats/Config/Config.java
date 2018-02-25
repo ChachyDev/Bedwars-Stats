126e1dc2f9385271a4f347bc3a3d9b44cfa7460c
Conor <31479541+ConorTheDev@users.noreply.github.com>
2018-02-25 22:40:07 +0000

    started working on the config

--- src/main/java/conortheoreo/bedwarsStats/Config/Config.java
+++ src/main/java/conortheoreo/bedwarsStats/Config/Config.java
@@ -1,4 +1,82 @@
 package conortheoreo.bedwarsStats.Config;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import conortheoreo.bedwarsStats.Main.MainClass;
+import net.minecraft.client.Minecraft;
+import org.apache.logging.log4j.LogManager;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Paths;
+
 public class Config {
+
+    public static File getConfigFile() {
+        return configFile;
+    }
+
+    private static final File configFile = new File(new File(String.valueOf(Minecraft.getMinecraft().mcDataDir.getPath()) + "/config/BedwarsStats"), "BedwarsStats.config");
+    private static JsonObject config;
+
+    public static boolean configExists() {
+        return exists(Config.configFile.getPath());
+    }
+
+    public static void loadSettings() {
+        if (configExists()) {
+            log("Config file exists! Reading...", new String[0]);
+            System.out.println("Config file exists! Reading..");
+            try {
+                final FileReader ex = new FileReader(Config.configFile);
+                final BufferedReader bufferedReader = new BufferedReader(ex);
+                final StringBuilder builder = new StringBuilder();
+                String currentLine;
+                while ((currentLine = bufferedReader.readLine()) != null) {
+                    builder.append(currentLine);
+                }
+                bufferedReader.close();
+                final String complete = builder.toString();
+                Config.config = new JsonParser().parse(complete).getAsJsonObject();
+            } catch (Exception var5) {
+                log("Could not write config! Saving...", new String[0]);
+                saveSettings();
+            }
+            System.out.println("reading...");
+            //read with dis
+            //MainClass.whatever= (Config.config.has("whatever") && Config.config.get("whatever").getAsBoolean());
+        } else {
+            log("Config does not exist! Saving...", new String[0]);
+            saveSettings();
+        }
+    }
+
+    public static void saveSettings() {
+        Config.config = new JsonObject();
+        try {
+            if (!Config.configFile.getParentFile().exists()) {
+                Config.configFile.getParentFile().mkdir();
+            }
+            Config.configFile.createNewFile();
+            final FileWriter ex = new FileWriter(Config.configFile);
+            final BufferedWriter bufferedWriter = new BufferedWriter(ex);
+            //Config.config.addProperty("whatever", MainClass.whatever);
+            bufferedWriter.write(Config.config.toString());
+            bufferedWriter.close();
+            ex.close();
+        } catch (Exception var2) {
+            log("Could not save config!", new String[0]);
+            var2.printStackTrace();
+        }
+    }
+
+    private static boolean exists(final String path) {
+        return Files.exists(Paths.get(path, new String[0]), new LinkOption[0]);
+    }
+
+    private static void log(final Object message, final String... replacements) {
+        LogManager.getLogger("FileUtils").info(String.format(String.valueOf(message), replacements));
+    }
 }
+
--- src/main/java/conortheoreo/bedwarsStats/Main/MainClass.java
+++ src/main/java/conortheoreo/bedwarsStats/Main/MainClass.java
@@ -1,5 +1,6 @@
 package conortheoreo.bedwarsStats.Main;
 
+import conortheoreo.bedwarsStats.Config.Config;
 import net.minecraft.client.Minecraft;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumChatFormatting;
@@ -10,6 +11,8 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import sun.awt.EmbeddedFrame;
 
+import java.io.File;
+
 @Mod(modid = MainClass.MODID, version = MainClass.VERSION, acceptedMinecraftVersions = MainClass.MCVERSION)
 public class MainClass
 {
@@ -19,12 +22,20 @@ public class MainClass
 
     public int totalStars;
 
+    private File ConfigFile = Config.getConfigFile();
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        System.out.println(ConfigFile);
+    }
 
     @SubscribeEvent
     public void render(RenderGameOverlayEvent event) {
         if (event.isCancelable() || event.type != RenderGameOverlayEvent.ElementType.EXPERIENCE) {
             return;
         }
+            System.out.println(ConfigFile);
             Minecraft.getMinecraft().fontRendererObj.drawString(EnumChatFormatting.GREEN + "Total Stars:" + EnumChatFormatting.WHITE + totalStars, 5, 5, 0);
         }
 
