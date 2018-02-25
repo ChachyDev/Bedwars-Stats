2baf33f30fe12ad87e4fb55990b953094779641f
Conor <31479541+ConorTheDev@users.noreply.github.com>
2018-02-25 21:45:39 +0000

    Fixed a typo lmao, also made it 1.8.9 only for now.

--- src/main/java/conortheoreo/bedwarsStats/Main/MainClass.java
+++ src/main/java/conortheoreo/bedwarsStats/Main/MainClass.java
@@ -5,11 +5,12 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = MainClass.MODID, version = MainClass.VERSION)
+@Mod(modid = MainClass.MODID, version = MainClass.VERSION, acceptedMinecraftVersions = MainClass.MCVERSION)
 public class MainClass
 {
     public static final String MODID = "bedwars-stats";
     public static final String VERSION = "1";
+    public static final String MCVERSION = "[1.8.9]";
 
     //todo
 }
