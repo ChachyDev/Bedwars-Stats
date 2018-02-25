c7ffbbf1f603408ff6ddad2481cbc196f82c8a07
Conor <31479541+ConorTheDev@users.noreply.github.com>
2018-02-25 18:44:29 +0000

    added main files & gradle.properties w/ 2gb ram

---
+++ src/main/java/conortheoreo/bedwarsStats/main/Main.java
@@ -0,0 +1,14 @@
+package conortheoreo.bedwarsStats.main;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Main.MODID, version = Main.VERSION)
+public class Main
+{
+    public static final String MODID = "BedwarsStats";
+    public static final String VERSION = "1";
+
+}
