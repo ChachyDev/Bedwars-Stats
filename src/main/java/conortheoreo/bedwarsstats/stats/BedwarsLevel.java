f4fea36ea1b0db9e5305715ca18552fd49bc5236
Conor <31479541+ConorTheDev@users.noreply.github.com>
2018-03-01 13:14:18 +0000

    added to ignore .key files for my api key

--- src/main/java/conortheoreo/bedwarsstats/stats/BedwarsLevel.java
+++ src/main/java/conortheoreo/bedwarsstats/stats/BedwarsLevel.java
@@ -1,5 +1,9 @@
 package conortheoreo.bedwarsstats.stats;
 
+import me.kbrewster.hypixelapi.HypixelAPI;
+import me.kbrewster.hypixelapi.player.HypixelPlayer;
+import me.kbrewster.hypixelapi.player.stats.bedwars.Bedwars;
+import me.kbrewster.hypixelapi.player.stats.uhc.UHC;
 import net.minecraft.client.Minecraft;
 import scala.runtime.AbstractFunction0$mcB$sp;
 
@@ -10,6 +14,19 @@ public class BedwarsLevel {
 
     public static void getBedwarsLevel() {
         System.out.println(username);
+    }
+    private static final String KEY = "KEY_HERE";
+
+    public static void main(String[] args) throws Exception {
+        HypixelAPI api = new HypixelAPI(KEY);
+        HypixelPlayer player = api.getPlayer(username);
+        Bedwars bw = player.getStats().getBedwars();
+        System.out.println(username + "'s Stats:");
+        System.out.println("Bedwars Level: " + bw.getExperienceNew());
+        if (player.getLastNick() != null) {
+            System.out.println("The player is currently nicked: " + player.getLastNick());
+        }
+
     }
 
 }
